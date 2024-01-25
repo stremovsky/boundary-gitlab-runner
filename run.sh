d8a7f4545e65e71dab626af94dc6bfc84923c9d8
Yuli <stremovsky@gmail.com>
2024-01-25 16:37:20 +0200

    fix path

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 docker kill runner || true
 docker rm runner
 USERDIR=`getent passwd $USER | cut -d: -f6`
-docker run --name runner -d --restart unless-stopped -v $USERDIR/.gitlab-runner/config:/etc/gitlab-runner --env-file .env runner
+docker run --name runner -d --restart unless-stopped -v $USERDIR/.gitlab-runner:/etc/gitlab-runner --env-file .env runner
--- shell.sh
+++ shell.sh
@@ -3,4 +3,4 @@
 docker kill runner || true
 docker rm runner
 USERDIR=`getent passwd $USER | cut -d: -f6`
-docker run --name runner -ti --rm -v $USERDIR/.gitlab-runner/config:/etc/gitlab-runner --env-file .env --entrypoint /bin/bash runner
+docker run --name runner -ti --rm -v $USERDIR/.gitlab-runner:/etc/gitlab-runner --env-file .env --entrypoint /bin/bash runner
