fa519ec7510ea5468cf9da187868211727b0a60c
Yuli <stremovsky@gmail.com>
2024-01-25 17:01:00 +0200

    publish port

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 docker kill runner || true
 docker rm runner
 USERDIR=`getent passwd $USER | cut -d: -f6`
-docker run --name runner -d --restart unless-stopped -v $USERDIR/.gitlab-runner:/etc/gitlab-runner --env-file .env runner
+docker run --name runner -d --restart unless-stopped -v $USERDIR/.gitlab-runner:/etc/gitlab-runner -p 127.0.0.1:9200:9200 --env-file .env runner
