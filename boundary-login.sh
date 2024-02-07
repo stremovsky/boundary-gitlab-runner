0b4aec3ad0d114d035917399b944efd1bba71965
Yuli <stremovsky@gmail.com>
2024-02-07 18:19:43 +0200

    calculate auth-method-id value

--- boundary-login.sh
+++ boundary-login.sh
@@ -5,7 +5,9 @@ if [ ! -f /root/.gnupg/pubring.kbx ]; then
   pass init mykey
 fi
 
-boundary authenticate password -auth-method-id ampw_0gZcQHhkPo -login-name $BOUNDARY_USER --password=env://BOUNDARY_PASSWORD
+AUTH_METHOD_ID=$(boundary auth-methods list -format json -keyring-type none | jq -rc '.items[] | select(.type == "password" and .scope_id =="global")' | jq -rc '.id')
+
+boundary authenticate password -auth-method-id $AUTH_METHOD_ID -login-name $BOUNDARY_USER --password=env://BOUNDARY_PASSWORD
 GLOBAL_SCOPE=`boundary scopes list -format=json | jq -r '.items[] | .id'`
 echo "dumping targets"
 boundary targets list -scope-id=$GLOBAL_SCOPE -recursive -format=json | jq -rc '.items[]| "boundary connect ssh -target-id "+.id+" #"+.name'
